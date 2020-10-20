414004c63f3237d4b75b3547fb470989b9409f35
hyunatic <habbo_armageddon@hotmail.com>
2020-10-20 17:49:22 +0800

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import LoginForm from './Components/Login/LoginForm';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import RegisterForm from './Components/Register/RegisterForm'
 import HomePage from './Components/HomePage/HomePage';
-import ProductPage from './Components/ProductPage/ProductPage';
 // import DatetimePage from  './Components/DateTimePage/datetimepage';
 import Donate from './Components/Donate/Donate';
 import "./index.css"
@@ -18,8 +17,7 @@ function App() {
           <Switch>
             <Route exact path="/" component={LoginForm}/>
             <Route exact path="/register" component={RegisterForm}/>
-            <Route exact path="/home" component={HomePage}/>
-            <Route exact path="/productpage" component={ProductPage}/>
+            <Route exact path="/home" component={RegisterForm}/>
             {/* <Route exact path="/datetimepage" component={DatetimePage}/> */}
             <Route exact path="/donate" component={Donate}/>
           </Switch>
