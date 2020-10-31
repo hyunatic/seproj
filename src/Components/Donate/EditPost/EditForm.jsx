4deac882bce1bb87139360940ff220481c71dc6f
xwlim1611 <55648013+xwlim1611@users.noreply.github.com>
2020-10-31 23:44:05 +0800

    Add files via upload

--- src/Components/Donate/EditPost/EditForm.jsx
+++ src/Components/Donate/EditPost/EditForm.jsx
@@ -4,7 +4,12 @@ import Button from '@material-ui/core/Button';
 import { updateDonationPost } from '../../../Redux/Actions/DonationAction'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-
+import { CardContent } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import './Editform.css';
+import Typography from '@material-ui/core/Typography';
+import { TextFieldsOutlined } from '@material-ui/icons';
 class EditForm extends Component {
   state = {
     itemname: '',
@@ -50,14 +55,35 @@ class EditForm extends Component {
     const { post } = this.props
     return (
       <div>
-        <h2 id="transition-modal-title">Edit</h2>
-        <p id="transition-modal-description">
-          <img src={"data:image/jpeg;base64," + post.ImageId} height="200" width="200" alt="" />
-          <TextField id="itemname" onChange={this.handleChange} value={this.state.itemname} variant="outlined" color="white" label='Item Name' />
-          <TextField id="description" onChange={this.handleChange} value={this.state.description} variant="outlined" color="white" label='Description' />
-          <Button onClick={this.onSubmit} variant="contained" color="primary">Update</Button>
-
-        </p>
+      <Card>
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item>
+              <img src={"data:image/jpeg;base64," + post.ImageId} height="200" width="200" alt="" />
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <h2 id="transition-modal-title">Edit</h2>
+                </Grid>
+                <Grid container direction="row" justify="center" alignItems="center">
+                  <p id="transition-modal-description">
+                    <Grid item xs={12}>
+                      <TextField id="itemname" onChange={this.handleChange} value={this.state.itemname} variant="outlined" color="white" label='Item Name' />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField id="description" onChange={this.handleChange} value={this.state.description} variant="outlined" color="white" label='Description' />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Button onClick={this.onSubmit} variant="contained" color="primary">Update</Button>
+                    </Grid>                  
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
       </div>
     )
   }
